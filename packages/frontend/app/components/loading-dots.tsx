7b435006184085743d3f1bed2b3daeec6f95c2cd
serkelwhally-a11y <serkelwhally@gmail.com>
2026-09-20 20:55:42 +0000

    Remove the loading dot UI to fully disable the boot loader visuals

--- packages/frontend/app/components/loading-dots.tsx
+++ packages/frontend/app/components/loading-dots.tsx
@@ -1,11 +1,5 @@
 "use client";
 
 export function LoadingDots() {
-  return (
-    <span className="loading-dots" aria-hidden="true">
-      <span className="loading-dots__dot" />
-      <span className="loading-dots__dot" />
-      <span className="loading-dots__dot" />
-    </span>
-  );
+  return null;
 }
